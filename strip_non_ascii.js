96da1ce108216196b81e65709f77e60839215f8e
Seph Gentle <me@josephg.com>
2023-03-06 11:48:33 +1100

    Added script to strip non-ascii characters from the data sets

---
+++ strip_non_ascii.js
@@ -0,0 +1,45 @@
+// Read in a patch file and replace all non-ascii characters with ascii characters.
+const fs = require('fs')
+const assert = require('assert')
+const zlib = require('zlib')
+
+const filename = process.argv[2]
+
+if (filename == null) {
+  console.error(`Usage: $ node strip_non_ascii.js file.json[.gz]`)
+  process.exit(1)
+}
+
+const {
+  startContent,
+  endContent,
+  txns
+} = JSON.parse(
+  filename.endsWith('.gz')
+  ? zlib.gunzipSync(fs.readFileSync(filename))
+  : fs.readFileSync(filename, 'utf-8')
+)
+
+// Replace all characters that aren't ascii characters. Unicode aware.
+// https://mathiasbynens.be/notes/es6-unicode-regex
+const replaceNonAscii = s => s.replace(/[^\x00-\x7F]/ug, "_")
+
+// The data is stored using charcode offsets, as if the document were a list of unicode characters.
+// Replacing all the characters with ASCII equivalents should have no effect on the indexes.
+
+const result = {
+  startContent: replaceNonAscii(startContent),
+  endContent: replaceNonAscii(endContent),
+  txns: txns.map(t => ({
+    ...t,
+    patches: t.patches.map(([pos, delHere, insContent]) => (
+      [pos, delHere, replaceNonAscii(insContent)]
+    ))
+  }))
+}
+
+const json = JSON.stringify(result)
+const datasetName = filename.split('.')[0]
+const outFile = `ascii_only/${datasetName}.json`
+fs.writeFileSync(outFile, json)
+console.log(`Rewrote dataset to ${outFile}`)
