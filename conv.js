e7f27ff06b6979f7788a66565475bcc5eabdae69
Seph Gentle <me@josephg.com>
2023-11-30 21:53:49 +1100

    Also converted the files in ascii-only

--- conv.js
+++ conv.js
@@ -6,33 +6,45 @@ const fs = require('fs')
 const assert = require('assert')
 const zlib = require('zlib')
 
-const filename = process.argv[2]
 
-if (filename == null) {
+
+function convert(filename) {
+  console.log('converting', filename)
+
+  const data = JSON.parse(
+    filename.endsWith('.gz')
+    ? zlib.gunzipSync(fs.readFileSync(filename))
+    : fs.readFileSync(filename, 'utf-8')
+  )
+
+  const result = {
+    ...data,
+    txns: data.txns.map(t => {
+      let timestamp = t.time
+      if (timestamp == null) throw Error('missing timestamp on txn')
+      return {
+        patches: t.patches.map(([a, b, c]) => [a, b, c, timestamp])
+      }
+    })
+  }
+
+  let out = JSON.stringify(result)
+  if (filename.endsWith('.gz')) out = zlib.gzipSync(out)
+  fs.writeFileSync(filename, out)
+
+  console.log('OK')
+}
+
+
+const files = process.argv.slice(2)
+
+if (files.length == 0) {
   console.error(`Usage: $ node check.js file.json[.gz]`)
   process.exit(1)
 }
 
-const data = JSON.parse(
-  filename.endsWith('.gz')
-  ? zlib.gunzipSync(fs.readFileSync(filename))
-  : fs.readFileSync(filename, 'utf-8')
-)
+files.forEach(convert)
 
-const result = {
-  ...data,
-  txns: data.txns.map(t => {
-    let timestamp = t.time
-    if (timestamp == null) throw Error('missing timestamp on txn')
-    return {
-      patches: t.patches.map(([a, b, c]) => [a, b, c, timestamp])
-    }
-  })
-}
-
-let out = JSON.stringify(result)
-if (filename.endsWith('.gz')) out = zlib.gzipSync(out)
-fs.writeFileSync(filename, out)
 
 // fs.writeFileSync(filename,
 
