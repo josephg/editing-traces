29d594fe4cc85d710708075cd34d02ae84c4730e
Seph Gentle <me@josephg.com>
2023-11-30 21:56:44 +1100

    Updated check and conv scripts to support multiple files at once

--- check.js
+++ check.js
@@ -1,54 +1,64 @@
 // Read in a patch file and check that the patches all apply correctly.
+
+// Test all with:
+// node check.js sequential_traces/*.json.gz sequential_traces/ascii_only/*.json.gz
+
 const fs = require('fs')
 const assert = require('assert')
 const zlib = require('zlib')
 
-const filename = process.argv[2]
+function check(filename) {
+  console.log('Checking', filename)
 
-if (filename == null) {
+  const {
+    startContent,
+    endContent,
+    txns
+  } = JSON.parse(
+    filename.endsWith('.gz')
+    ? zlib.gunzipSync(fs.readFileSync(filename))
+    : fs.readFileSync(filename, 'utf-8')
+  )
+
+  let content = startContent
+
+  console.log('applying', txns.length, 'txns...')
+  console.time('apply')
+  let lastTime = 0
+  for (let i = 0; i < txns.length; i++) {
+    if (i % 10000 == 0) console.log(i)
+    const {patches} = txns[i]
+    // assert(time != null)
+    assert(patches != null)
+    assert(patches.length > 0)
+
+    // let t = new Date(time).getTime()
+    // assert(t >= lastTime)
+    // lastTime = t
+
+    for (const [pos, delHere, insContent, timestamp] of patches) {
+      // if (insContent != '') console.log('Ins', {pos, content: insContent})
+      // if (delHere != 0) console.log('Del', {pos, len: delHere})
+      assert(content.length >= pos + delHere)
+      const before = content.slice(0, pos)
+      const after = content.slice(pos + delHere)
+      content = before + insContent + after
+
+      if (new Date(timestamp).toString() === 'Invalid Date') throw Error('invalid date')
+    }
+  }
+  console.timeEnd('apply')
+
+  assert.strictEqual(content, endContent)
+  console.log(`Looking good - ${txns.length} apply cleanly.`)
+}
+
+
+const files = process.argv.slice(2)
+
+if (files.length == 0) {
   console.error(`Usage: $ node check.js file.json[.gz]`)
   process.exit(1)
 }
 
-const {
-  startContent,
-  endContent,
-  txns
-} = JSON.parse(
-  filename.endsWith('.gz')
-  ? zlib.gunzipSync(fs.readFileSync(filename))
-  : fs.readFileSync(filename, 'utf-8')
-)
-
-let content = startContent
-
-console.log('applying', txns.length, 'txns...')
-console.time('apply')
-let lastTime = 0
-for (let i = 0; i < txns.length; i++) {
-  if (i % 10000 == 0) console.log(i)
-  const {patches} = txns[i]
-  // assert(time != null)
-  assert(patches != null)
-  assert(patches.length > 0)
-
-  // let t = new Date(time).getTime()
-  // assert(t >= lastTime)
-  // lastTime = t
-
-  for (const [pos, delHere, insContent, timestamp] of patches) {
-    // if (insContent != '') console.log('Ins', {pos, content: insContent})
-    // if (delHere != 0) console.log('Del', {pos, len: delHere})
-    assert(content.length >= pos + delHere)
-    const before = content.slice(0, pos)
-    const after = content.slice(pos + delHere)
-    content = before + insContent + after
-
-    if (new Date(timestamp).toString() === 'Invalid Date') throw Error('invalid date')
-  }
-}
-console.timeEnd('apply')
-
-assert.strictEqual(content, endContent)
-
-console.log(`Looking good - ${txns.length} apply cleanly.`)
+files.forEach(check)
--- conv.js
+++ conv.js
@@ -39,42 +39,8 @@ function convert(filename) {
 const files = process.argv.slice(2)
 
 if (files.length == 0) {
-  console.error(`Usage: $ node check.js file.json[.gz]`)
+  console.error(`Usage: $ node conv.js file.json[.gz]`)
   process.exit(1)
 }
 
 files.forEach(convert)
-
-
-// fs.writeFileSync(filename,
-
-// let content = startContent
-
-// console.log('applying', txns.length, 'txns...')
-// console.time('apply')
-// let lastTime = 0
-// for (let i = 0; i < txns.length; i++) {
-//   if (i % 10000 == 0) console.log(i)
-//   const {patches} = txns[i]
-//   // assert(time != null)
-//   assert(patches != null)
-//   assert(patches.length > 0)
-
-//   // let t = new Date(time).getTime()
-//   // assert(t >= lastTime)
-//   // lastTime = t
-
-//   for (const [pos, delHere, insContent] of patches) {
-//     // if (insContent != '') console.log('Ins', {pos, content: insContent})
-//     // if (delHere != 0) console.log('Del', {pos, len: delHere})
-//     assert(content.length >= pos + delHere)
-//     const before = content.slice(0, pos)
-//     const after = content.slice(pos + delHere)
-//     content = before + insContent + after
-//   }
-// }
-// console.timeEnd('apply')
-
-// assert.strictEqual(content, endContent)
-
-// console.log(`Looking good - ${txns.length} apply cleanly.`)
